f6a38aa9d2c219e7431d9ff8689eb996a59c7b2f
rahulkuanr <47236527+rahulkuanr@users.noreply.github.com>
2022-07-16 16:35:07 +0530

    first commit

---
+++ models/shortUrl.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const shortid = require('shortid');
+
+
+const shortUrlSchema = new mongoose.Schema({
+    full: {
+        type: String,
+        required: true,
+    },
+    short: {
+        type: String,
+        required: true,
+        default: shortid.generate
+    },
+    clicks: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+});
+
+const ShortUrl = mongoose.model('ShortUrl', shortUrlSchema);
+
+module.exports = ShortUrl;
